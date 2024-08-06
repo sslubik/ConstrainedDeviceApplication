db9bc79e2cb3aa4a6052da9be015751439459a18
sslubik <szymonslubik@gmail.com>
2024-08-06 21:36:42 +0200

    Fix imports

--- app/src/main/java/org/cda/Main.java
+++ app/src/main/java/org/cda/Main.java
@@ -1,13 +1,14 @@
 package org.cda;
 
 import org.cda.app.App;
-import org.cda.system.SystemPerformanceManager;
+import org.cda.data.SystemData;
+import org.cda.managers.SystemPerformanceManager;
 
 public class Main {
     public static void main(String[] args) {
         App gda = new App(
                 args,
-                new SystemPerformanceManager());
+                new SystemPerformanceManager(new SystemData()));
 
         gda.start();
         gda.stop();
