318d98b7184658eeff94bfcc5139eff4088749c3
sslubik <szymonslubik@gmail.com>
2024-08-09 20:47:11 +0200

    Create class responsible for storing device location data

---
+++ app/src/main/java/org/cda/data/LocationData.java
@@ -0,0 +1,13 @@
+package org.cda.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LocationData {
+
+    private double latitude;
+    private double longitude;
+    private double elevation;
+}
